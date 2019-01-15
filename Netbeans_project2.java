02bc1c75dc1fd5369b09cb7aaadd3fd7cb7dfa7e
bocekkafasi <46712948+bocekkafasi@users.noreply.github.com>
2019-01-15 17:12:34 +0300

    Add files via upload

---
+++ Netbeans_project2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeans_project1;
+
+/**
+ *
+ * @author vuralfat
+ */
+public class Netbeans_project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 0;i<10;i++)//loop variable
+        {
+            System.out.println("Mesaj"+i);
+        }
+        int i=0;
+        while(i<10){
+            System.out.println("Mesaj"+i);
+            i++;
+        }
+        
+        i=0;
+        do{
+        System.out.println("Son mesaj"+i);
+        i++;
+        }while(i<10);
+    }
+    }
+    
+
